b39c96700d386df6b18cf8a60c05ab0747d3513d
pyjavathon <86497379+pyjavathon@users.noreply.github.com>
2021-12-10 22:42:26 +0900

    5강 다차원 배열하는 중

--- src/part05/exam01/ArrayInarrayExample.java
+++ src/part05/exam01/ArrayInarrayExample.java
@@ -6,14 +6,28 @@ public class ArrayInarrayExample {
 		
 		int[][] mathScores = new int [2][3];
 		for(int i = 0; i<mathScores.length; i++) {
-			for(int k = 0; k<mathScores.length; k++) {
+			for(int k = 0; k<mathScores[i].length; k++) {
 				System.out.println("mathScores[" + i + "]["+k+"]="+mathScores[i][k]);
+			}
+		}
+		System.out.println();//띄어쓰기하려고
+		
+		int[][] englishScores = new int [2][];
+		englishScores[0] = new int[2];
+		englishScores[1] = new int[3];
+		for(int i = 0; i<englishScores.length; i++) {
+			for(int k = 0; k<englishScores[i].length; k++) {
+				System.out.println("englishScores["+i+"][" + k + "]=" + englishScores[i][k]);
 			}
 		}
 		System.out.println();
 		
-		int[][] englishScores = new int [2][];
-		englishScores[0] = new int[2];
+		int [][] javaScores = {{95,80},{92,96,80}};
+		for(int i = 0; i<javaScores.length; i++) {
+			for(int k = 0; k<javaScores[i].length; k++) {
+				System.out.println("javaScores[" + i + "][" + k + " ]= " + javaScores[i][k]);
+			}
+		}
 	}
 
 }
