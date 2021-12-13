5586804a5d43476dfd98416efaca78e5d671d4da
pyjavathon <86497379+pyjavathon@users.noreply.github.com>
2021-12-13 13:48:51 +0900

    오전 공부 commit! 10강 끝

---
+++ src/part10/exception01/NullPointerException.java
@@ -0,0 +1,10 @@
+package part10.exception01;
+
+public class NullPointerException {
+
+	public static void main(String[] args) {
+		String data = null;
+		System.out.println(data.toString());
+	}
+
+}
